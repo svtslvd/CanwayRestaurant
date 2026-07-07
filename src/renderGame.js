c523fd088ba3b85e90d67fb5339c35042d65a1a7
svtslvd <sviatoslavdavydovych@gmail.com>
2026-07-07 05:49:55 +0000

    min adaptability

--- src/renderGame.js
+++ src/renderGame.js
@@ -1,7 +1,14 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-const CELL_SIZE = 6; 
+const screenHeight = window.innerHeight;
+const CELL_SIZE = Math.floor(screenHeight/100);
+document.documentElement.style.setProperty('--screen-height', `${CELL_SIZE*100}px`);
+
+const finalSize = CELL_SIZE*100;
+canvas.width = finalSize;
+canvas.height = finalSize;
+
 function renderLife(matrix) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
